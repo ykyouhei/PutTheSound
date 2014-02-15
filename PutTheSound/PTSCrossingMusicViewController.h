e2a47289b17d804d1463468af0d9b51ed9925911
tochiba <c104kn@gmail.com>
2014-02-16 04:43:07 +0900

    曲一覧画面生成

---
+++ PutTheSound/PTSCrossingMusicViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTSCrossingMusicViewController.h
+//  PutTheSound
+//
+//  Created by 千葉 俊輝 on 2014/02/16.
+//  Copyright (c) 2014年 Toshiki Chiba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTSCrossingMusicViewController : UIViewController
+
+@end
---
+++ PutTheSound/PTSCrossingMusicViewController.m
@@ -0,0 +1,38 @@
+//
+//  PTSCrossingMusicViewController.m
+//  PutTheSound
+//
+//  Created by 千葉 俊輝 on 2014/02/16.
+//  Copyright (c) 2014年 Toshiki Chiba. All rights reserved.
+//
+
+#import "PTSCrossingMusicViewController.h"
+
+@interface PTSCrossingMusicViewController ()
+
+@end
+
+@implementation PTSCrossingMusicViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
